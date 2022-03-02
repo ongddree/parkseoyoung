3e4409b982205f707a5ef7704a4c557f5e89628b
ongddree <ongddree@gmail.com>
2022-03-02 23:39:27 +0900

    ✨ project list 추가

---
+++ src/constants/projectList.ts
@@ -0,0 +1,108 @@
+export const PROJECT_LIST = [
+{
+  name:"가위바위보",
+  desc:"로봇을 상대로 이겨라! 재밌는 가위바위보 게임",
+  date:"2021.12",
+  stacks: ["HTML","CSS","Javascript"],
+  links:["github","view"],
+},
+{
+  name: "벤딩머신",
+  desc: "돈을 넣으면 시원한 콜라를 마실 수 있는 벤딩머신",
+  date: "2021.12",
+  stacks: ["HTML","CSS","Javascript"],
+  links: [
+    {name: "github",
+    url: "https://github.com/ongddree/Project-With-VanillaJS/tree/main/vending-machine"
+  },
+  {
+    name: "view",
+    url:"https://cola-vending-machine.vercel.app/"
+  },
+  {
+    name: "blog",
+    url:"https://velog.io/@ongddree/%EB%B2%A4%EB%94%A9%EB%A8%B8%EC%8B%A0-%EC%BD%9C%EB%9D%BC-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EA%B4%80%EB%A6%AC-%EB%B0%8F-%ED%92%88%EC%A0%88-%EA%B8%B0%EB%8A%A5"
+  }
+],
+},
+{
+  name: "퓨처라마 웹사이트",
+  desc: "오픈 API를 활용한 퓨처라마 데이터 사이트",
+  date: "2021. 12 ~ 2022. 1",
+  stacks: ["React","Next.js","Typescript"],
+  links: [
+  {
+    name: "github",
+    url: "https://github.com/ongddree/futurama.vercel.com",
+  },
+  {
+    name: "view",
+    url:"https://futurama-vercel-com-six.vercel.app",
+  },
+  {
+    name: "blog",
+    url:"https://velog.io/@ongddree"
+  }
+],
+},
+{
+  name: "브래드피트",
+  desc: "빵에 관한 정보를 공유하는 SNS",
+  date: "2021. 12 ~ 2022. 1",
+  stacks: ["React","EmotionJS"],
+  links: [
+  {
+    name: "github",
+    url: "https://github.com/ongddree/bread-fit-react",
+  },
+  {
+    name: "view",
+    url:"https://bread-fit-react.vercel.app/",
+  },
+  {
+    name: "blog",
+    url:"https://velog.io/@ongddree"
+  }
+],
+},
+{
+  name: "스페이스 로그",
+  desc: "나의 이야기를 기록하는 블로그 서비스",
+  date: "2022.1 ~ 2022. 3",
+  stacks: ["Next.js", "EmotionJS", "Typescript", "Strapi"],
+  links: [
+  {
+    name: "github",
+    url: "https://github.com/ongddree/bread-fit-react",
+  },
+  {
+    name: "view",
+    url:"https://bread-fit-react.vercel.app/",
+  },
+  {
+    name: "blog",
+    url:"https://velog.io/@ongddree"
+  }
+],
+},
+{
+  name: "밑미",
+  desc: "'나를 돌보는 시간', 밑미 웹서비스",
+  date: "2022.2 ~ 2022. 3",
+  stacks: ["Next.js", "EmotionJS", "Typescript", "Strapi"],
+  links: [
+  {
+    name: "github",
+    url: "",
+  },
+  {
+    name: "view",
+    url:"",
+  },
+  {
+    name: "blog",
+    url:"https://velog.io/@ongddree"
+  }
+],
+},
+];
